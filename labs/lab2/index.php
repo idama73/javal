39c560be244106ce27b5a5abd3ff1df5bf9e068f
Juan Avalos <javalos@csumb.edu>
2018-02-05 22:59:02 +0000

    ading lab 2

---
+++ labs/lab2/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Lab 2: 777 Slot Machine </title>
+        <meta charset="utf-8"/>
+    </head>
+    <body>
+        
+        <img src="img/bg7.jpg" />
+    </body>
+</html>
