9cc4c98d419e6f93ae8216e5d215db1d749ee0cc
Juan Avalos <javalos@csumb.edu>
2018-04-01 00:04:10 +0000

    final lab 6

--- labs/lab6/index.php
+++ labs/lab6/index.php
@@ -41,7 +41,8 @@
             $sql = "SELECT * FROM om_product WHERE 1";
             
             if (!empty($_GET['product'])) { //checks whether user has typed something in the "Product" text box
-                 $sql .=  " AND productName LIKE :productName";
+                 $sql .=  " AND productName OR productDescription LIKE :productName";
+                 //$sql .= " AND productDescription LIKE :productDescription";
                  $namedParameters[":productName"] = "%" . $_GET['product'] . "%";
             }
                   
